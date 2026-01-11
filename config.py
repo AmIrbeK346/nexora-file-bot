f2816c84ffa8790734cf18bbb67baecc5e1e7dc1
AmIrbeK346 <107676007+AmIrbeK346@users.noreply.github.com>
2026-01-12 03:16:13 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,20 @@
 import os
-import tempfile
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
-POPPLER_PATH = r'C:\Users\iskan\Downloads\poppler-25.12.0\Library\bin' 
+ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
 
-# DIQQAT: .exe emas, .com bo'lishi shart!
-LIBREOFFICE_PATH = r'C:\Program Files\LibreOffice\program\soffice.com'
 
-# Windows vaqtinchalik papkasi
-TEMP_DIR = os.path.join(tempfile.gettempdir(), "telegram_file_bot")
+if os.name == 'nt':  # Windows uchun
+    LIBREOFFICE_PATH = r'C:\Program Files\LibreOffice\program\soffice.com'
+    POPPLER_PATH = r'C:\Users\iskan\Downloads\poppler-25.12.0\Library\bin'
+else:  # Linux (Railway/Server) uchun
+    LIBREOFFICE_PATH = 'soffice'
+    POPPLER_PATH = None  
+
+TEMP_DIR = "/tmp/telegram_file_bot" # Serverda /tmp papkasi xavfsizroq
 if not os.path.exists(TEMP_DIR):
     os.makedirs(TEMP_DIR)
