04b577a5b439b27fc01610f97614ca1a47aae900
AmIrbeK346 <107676007+AmIrbeK346@users.noreply.github.com>
2026-01-13 20:53:23 +0500

    Update i18n.py

--- middlewares/i18n.py
+++ middlewares/i18n.py
@@ -1,18 +1,13 @@
 from aiogram import BaseMiddleware
 from locales.messages import MESSAGES
-
-
-user_lang_db = {}
+from utils.db import db # Bazani import qilamiz
 
 class L10nMiddleware(BaseMiddleware):
     async def __call__(self, handler, event, data):
         user = data.get("event_from_user")
-        
         if user:
-            user_id = user.id
-            # Foydalanuvchi tili bazada bo'lmasa, default 'uz'
-            lang = user_lang_db.get(user_id, 'uz')
+            # Lug'at o'rniga bazadan o'qiymiz
+            lang = db.get_language(user.id)
             data['lang'] = lang
             data['l10n'] = MESSAGES.get(lang, MESSAGES['uz'])
-        
         return await handler(event, data)
