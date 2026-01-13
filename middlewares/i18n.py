5156a82798299dbfdf781b199310aa3e9ebd22d4
AmIrbeK346 <107676007+AmIrbeK346@users.noreply.github.com>
2026-01-13 21:07:26 +0500

    Update i18n.py

--- middlewares/i18n.py
+++ middlewares/i18n.py
@@ -6,7 +6,7 @@ class L10nMiddleware(BaseMiddleware):
     async def __call__(self, handler, event, data):
         user = data.get("event_from_user")
         if user:
-            # Lug'at o'rniga bazadan o'qiymiz
+            # Endi lug'atdan emas, bazadan o'qiymiz
             lang = db.get_language(user.id)
             data['lang'] = lang
             data['l10n'] = MESSAGES.get(lang, MESSAGES['uz'])
