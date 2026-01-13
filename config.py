df6c550498c0bbc681cc2ec3dd419e39eced9ede
AmIrbeK346 <107676007+AmIrbeK346@users.noreply.github.com>
2026-01-13 16:20:05 +0500

    Update config.py

--- config.py
+++ config.py
@@ -7,17 +7,17 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
 
-
+# Faqat tizim yo'llari qoladi
 if os.name == 'nt':  # Windows uchun
     LIBREOFFICE_PATH = r'C:\Program Files\LibreOffice\program\soffice.com'
     POPPLER_PATH = r'C:\Users\iskan\Downloads\poppler-25.12.0\Library\bin'
-else:  # Linux (Railway/Server) uchun
+else:  # Linux (Railway) uchun
     LIBREOFFICE_PATH = 'soffice'
     POPPLER_PATH = None  
-if os.name == 'nt': # Windows uchun (mahalliy test)
-    DB_PATH = "bot_database.db"
-else: # Linux (Railway) uchun doimiy yo'l
-    DB_PATH = "/data/bot_database.db"
-TEMP_DIR = "/tmp/telegram_file_bot" # Serverda /tmp papkasi xavfsizroq
+
+# Bazaga tegishli eski DB_PATH qismlari o'chirildi, 
+# chunki db.py DATABASE_URL dan foydalanadi.
+
+TEMP_DIR = "/tmp/telegram_file_bot" 
 if not os.path.exists(TEMP_DIR):
     os.makedirs(TEMP_DIR)
