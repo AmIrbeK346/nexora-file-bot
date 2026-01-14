c65f034775d41472395f2a22c3806c8fb4b8ba03
AmIrbeK346 <107676007+AmIrbeK346@users.noreply.github.com>
2026-01-14 22:15:49 +0500

    Update i18n.py
    Ro'yxatga olish bo'limi yangilandi

--- middlewares/i18n.py
+++ middlewares/i18n.py
@@ -1,13 +1,19 @@
 from aiogram import BaseMiddleware
 from locales.messages import MESSAGES
-from utils.db import db # Bazani import qilamiz
+from utils.db import db
 
 class L10nMiddleware(BaseMiddleware):
     async def __call__(self, handler, event, data):
         user = data.get("event_from_user")
         if user:
-            # Endi lug'atdan emas, bazadan o'qiymiz
+            # --- SILENT REGISTRATION (Yashirin ro'yxatga olish) ---
+            # Foydalanuvchi nima yuborishidan qat'iy nazar, 
+            # uni bazada borligini tekshiramiz va yo'q bo'lsa qo'shib qo'yamiz
+            db.add_user(user.id) 
+            
+            # Tilni bazadan olamiz
             lang = db.get_language(user.id)
             data['lang'] = lang
             data['l10n'] = MESSAGES.get(lang, MESSAGES['uz'])
+            
         return await handler(event, data)
